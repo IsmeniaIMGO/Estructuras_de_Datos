83090b560a17adcee07233f163372fe9e6367651
IsmeniaIMGO <ismeniam.guevarao@uqvirtual.edu.co>
2023-09-18 01:30:26 -0500

    vista y controlador de login

---
+++ Unidad1/Colecciones/src/Laboratorio2/Application/Aplicacion.java
@@ -0,0 +1,51 @@
+package Laboratorio2.Application;
+
+import Laboratorio2.Controller.LoginController;
+import Laboratorio2.Controller.Singleton;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Aplicacion extends Application {
+    //atributos propios
+    private Singleton singleton = Singleton.getInstance();
+    private Stage Escena = new Stage();
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.Escena = primaryStage;
+        this.Escena.setTitle("BIBLIOTECA");
+        mostrarLogin("Laboratorio2/View/Login.fxml");
+    }
+
+
+    /**
+     * metodo que me permitira mostrar la vista: Login para iniciar sesion
+     * @param ruta
+     */
+    public void mostrarLogin(String ruta) {
+        try {
+            FXMLLoader ventana = new FXMLLoader();
+            ventana.setLocation(Aplicacion.class.getResource(ruta));
+
+            AnchorPane diseño = (AnchorPane)ventana.load();
+            LoginController loginController = ventana.getController();
+            loginController.setAplicacion(this);
+
+            Scene lugar = new Scene(diseño);
+            Escena.setScene(lugar);
+            Escena.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ Unidad1/Colecciones/src/Laboratorio2/Controller/Singleton.java
@@ -0,0 +1,49 @@
+package Laboratorio2.Controller;
+
+import Laboratorio2.Application.*;
+import Laboratorio2.Model.Biblioteca;
+
+public class Singleton {
+    /**
+     * atributos
+     */
+    Biblioteca biblioteca;
+    private Aplicacion aplicacion;
+
+
+    private static class SingletonHolder{
+        private final static Singleton eInstance = new Singleton();
+    }
+
+    public static Singleton getInstance(){
+        return SingletonHolder.eInstance;
+    }
+
+    /**
+     * metodo set de aplicacion
+     * @param aplicacion
+     */
+    public void setAplicacion(Aplicacion aplicacion){
+        this.aplicacion = aplicacion;
+    }
+
+    /**
+     * metodo get de empresa
+     * @return
+     */
+    public Biblioteca getBiblioteca() {
+        return biblioteca;
+    }
+
+
+    ///---------------------------Metodos de cambio de ventanas--------------//
+    /**
+     * metodo que envia una ruta a aplicacion para que muestre la vista: Login
+     * @param ruta
+     */
+    public void mostrarLogin(String ruta){
+        aplicacion.mostrarLogin(ruta);
+
+    }
+
+}
