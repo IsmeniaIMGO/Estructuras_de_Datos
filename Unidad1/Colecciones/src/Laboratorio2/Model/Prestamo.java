66916c87597ce1ca44255d1864d784a637d7fc73
IsmeniaIMGO <ismeniam.guevarao@uqvirtual.edu.co>
2023-09-18 00:26:02 -0500

    clase Prestamo y destalle prestamo

--- Unidad1/Colecciones/src/Laboratorio2/Model/Prestamo.java
+++ Unidad1/Colecciones/src/Laboratorio2/Model/Prestamo.java
@@ -1,4 +1,96 @@
 package Laboratorio2.Model;
 
+import java.util.HashMap;
+import java.util.Objects;
+
 public class Prestamo {
+    //Atributos
+    private String id;
+    private Usuario usuario;
+    private HashMap<String, DetallePrestamo> listaDetallePrestamo = new HashMap<>();
+
+    //Constructor sin atributos
+    public Prestamo() {
+    }
+
+    //Constructor con atributos
+    public Prestamo(String id, Usuario usuario, HashMap<String, DetallePrestamo> listaDetallePrestamo) {
+        this.id = id;
+        this.usuario = usuario;
+        this.listaDetallePrestamo = listaDetallePrestamo;
+    }
+
+    //Getters y Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public HashMap<String, DetallePrestamo> getListaDetallePrestamo() {
+        return listaDetallePrestamo;
+    }
+
+    public void setListaDetallePrestamo(HashMap<String, DetallePrestamo> listaDetallePrestamo) {
+        this.listaDetallePrestamo = listaDetallePrestamo;
+    }
+
+    //Metodos equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Prestamo prestamo = (Prestamo) o;
+        return Objects.equals(id, prestamo.id);
+    }
+
+    // hashcode
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    //metodo toString
+
+
+    @Override
+    public String toString() {
+        return "Prestamo{" +
+                "id='" + id + '\'' +
+                ", usuario=" + usuario +
+                ", listaDetallePrestamo=" + listaDetallePrestamo +
+                '}';
+    }
+
+
+
+    //Metodo para crear un detalle de prestamo
+    public void crearDetallePrestamo(String id, Libro libro){
+        DetallePrestamo detallePrestamo = new DetallePrestamo(id, libro);
+        listaDetallePrestamo.put(id, detallePrestamo);
+    }
+
+    //Metodo para eliminar un detalle de prestamo
+    public void eliminarDetallePrestamo(String id){
+        listaDetallePrestamo.remove(id);
+    }
+
+    //Metodo para buscar un detalle de prestamo
+    public DetallePrestamo buscarDetallePrestamo(String id){
+        return listaDetallePrestamo.get(id);
+    }
+
+
+
 }
