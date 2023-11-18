b6e30f186509cc1b0b6cee5c5997e93dcc83a180
IsmeniaIMGO <ismeniam.guevarao@uqvirtual.edu.co>
2023-11-18 15:39:24 -0500

    clase DetalleActividad y DetalleProceso

--- proyectoFinal/empresa_ws/Empresa/src/model/DetalleActividad.java
+++ proyectoFinal/empresa_ws/Empresa/src/model/DetalleActividad.java
@@ -1,4 +1,60 @@
 package model;
 
+import java.util.Objects;
+
 public class DetalleActividad {
+    //atributos
+    private String id;
+    private Tarea tarea;
+
+    //Constructor vacio
+    public DetalleActividad() {
+    }
+
+    //Constructor con atributos
+    public DetalleActividad(String id, Tarea tarea) {
+        this.id = id;
+        this.tarea = tarea;
+    }
+
+    //Getters y Setters
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public Tarea getTarea() {
+        return tarea;
+    }
+    public void setTarea(Tarea tarea) {
+        this.tarea = tarea;
+    }
+
+    //metodo equals
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DetalleActividad that = (DetalleActividad) o;
+        return Objects.equals(id, that.id);
+    }
+
+    //Metodo hashCode
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    //Metodo toString
+    @Override
+    public String toString() {
+        return "DetalleActividad{" +
+                "id='" + id + '\'' +
+                ", tarea=" + tarea +
+                '}';
+    }
+
+
 }
--- proyectoFinal/empresa_ws/Empresa/src/model/DetalleProceso.java
+++ proyectoFinal/empresa_ws/Empresa/src/model/DetalleProceso.java
@@ -1,4 +1,65 @@
 package model;
 
+import javafx.scene.AccessibleRole;
+
+import java.util.Objects;
+
 public class DetalleProceso {
+    // Atributos
+    private String id;
+    private Actividad actividad;
+
+    // Constructor vacio
+    public DetalleProceso() {
+    }
+
+    // Constructor con atributos
+    public DetalleProceso(String id, Actividad actividad) {
+        this.id = id;
+        this.actividad = actividad;
+    }
+
+    // Getters y Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Actividad getActividad() {
+        return actividad;
+    }
+
+    public void setActividad(Actividad actividad) {
+        this.actividad = actividad;
+    }
+
+    //metodo equals
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DetalleProceso that = (DetalleProceso) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    //Metodo toString
+    @Override
+    public String toString() {
+        return "DetalleProceso{" +
+                "id='" + id + '\'' +
+                ", actividad=" + actividad +
+                '}';
+    }
+
+
 }
